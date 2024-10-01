e7bea5f7be82301b37d4d361ddb9285af10e8f49
Akashnagraj <akashnagraj77@gmail.com>
2024-10-01 13:32:49 +0530

    This is my video_05

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:codepur_hindi/screens/homepage.dart';
 import 'package:codepur_hindi/screens/login_page.dart';
+import 'package:codepur_hindi/utils/myroutes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,7 +22,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (context) => LoginPage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute : (context)=> CodePurHindi(), 
       },
     );
   }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:codepur_hindi/utils/myroutes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,67 +7,78 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  maxRadius: 150.0,
-                  backgroundImage: AssetImage("assets/images/login.png"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Welcome, Sangeeth Aunty",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 25, right: 25, top: 10, bottom: 10),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Username",
-                          hintText: "Sangeetha Venkatesh, the Great",
-                          prefixIcon: Icon(Icons.verified_user),
-                          border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 4.0),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          hintText: "S2Y",
-                          prefixIcon: Icon(Icons.password),
-                          border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 4.0),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text("Login"))
-                    ],
+          child: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                )
-              ],
+                  CircleAvatar(
+                    maxRadius: 150.0,
+                    backgroundImage: AssetImage("assets/images/login.png"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Welcome, Sangeeth Aunty",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 25, right: 25, top: 10, bottom: 10),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Username",
+                            hintText: "Sangeetha Venkatesh, the Great",
+                            prefixIcon: Icon(Icons.verified_user),
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.black, width: 4.0),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            hintText: "S2Y",
+                            prefixIcon: Icon(Icons.password),
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.black, width: 4.0),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, MyRoutes.homeRoute);
+                          },
+                          child: Text("Login"),
+                          style: TextButton.styleFrom(fixedSize: Size(120, 40)),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
