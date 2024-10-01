c288c33975c0815dcd2c98e7b997f8312b7b266a
Akashnagraj <akashnagraj77@gmail.com>
2024-10-01 12:59:52 +0530

    Video 4 done and Dusted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:codepur_hindi/screens/homepage.dart';
 import 'package:codepur_hindi/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.pink,
+        fontFamily: GoogleFonts.lato(fontSize: 30).fontFamily,
+      ),
       routes: {
         "/": (context) => LoginPage(),
         "/login": (context) => LoginPage(),
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -4,9 +4,71 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login Page"),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  maxRadius: 150.0,
+                  backgroundImage: AssetImage("assets/images/login.png"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Welcome, Sangeeth Aunty",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 10, bottom: 10),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          hintText: "Sangeetha Venkatesh, the Great",
+                          prefixIcon: Icon(Icons.verified_user),
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 4.0),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "S2Y",
+                          prefixIcon: Icon(Icons.password),
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 4.0),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ElevatedButton(onPressed: () {}, child: Text("Login"))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
