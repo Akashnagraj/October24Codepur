29dc7303054f4fbee7d05d73106b17476cd8ac9f
Akashnagraj <akashnagraj77@gmail.com>
2024-10-01 11:10:35 +0530

    First Commit, checking

---
+++ android/app/src/main/kotlin/com/example/codepur_hindi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codepur_hindi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CodePurHindi extends StatelessWidget {
+  const CodePurHindi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink.shade200,
+    );
+  }
+}
