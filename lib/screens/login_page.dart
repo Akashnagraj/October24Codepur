d56d244f1d9530cf726ea345d74683cf3183c817
Akashnagraj <akashnagraj77@gmail.com>
2024-10-01 14:33:12 +0530

    This is day 5, video 5 done

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,7 +1,20 @@
 import 'package:codepur_hindi/utils/myroutes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool isTapped = false;
+
+  void changePage() async {
+    await Future.delayed((Duration(seconds: 2)));
+    Navigator.pushNamed(context, MyRoutes.homeRoute);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +38,7 @@ class LoginPage extends StatelessWidget {
                     height: 20,
                   ),
                   Text(
-                    "Welcome, Sangeeth Aunty",
+                    "Welcome $name",
                     style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
@@ -47,6 +60,11 @@ class LoginPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20),
                             ),
                           ),
+                          onChanged: (value) {
+                            setState(() {
+                              name = value;
+                            });
+                          },
                         ),
                         SizedBox(
                           height: 15,
@@ -56,7 +74,9 @@ class LoginPage extends StatelessWidget {
                           decoration: InputDecoration(
                             labelText: "Password",
                             hintText: "S2Y",
-                            prefixIcon: Icon(Icons.password),
+                            prefixIcon: Icon(
+                              Icons.password,
+                            ),
                             border: OutlineInputBorder(
                               borderSide:
                                   BorderSide(color: Colors.black, width: 4.0),
@@ -67,13 +87,47 @@ class LoginPage extends StatelessWidget {
                         SizedBox(
                           height: 20,
                         ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, MyRoutes.homeRoute);
+
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              isTapped = !isTapped;
+                              changePage();
+                            });
                           },
-                          child: Text("Login"),
-                          style: TextButton.styleFrom(fixedSize: Size(120, 40)),
+                          child: AnimatedContainer(
+                            height: isTapped ? 40 : 40,
+                            width: isTapped ? 40 : 120,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              shape: isTapped
+                                  ? BoxShape.circle
+                                  : BoxShape.rectangle,
+                              color: Colors.pink,
+                            ),
+                            duration: Duration(seconds: 2),
+                            child: isTapped
+                                ? Icon(
+                                    Icons.check,
+                                    color: Colors.white,
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                          ),
                         )
+
+                        // ElevatedButton(
+                        //   onPressed: () {
+                        //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        //   },
+                        //   child: Text("Login"),
+                        //   style: TextButton.styleFrom(fixedSize: Size(120, 40)),
+                        // ),
                       ],
                     ),
                   )
